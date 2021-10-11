2f7b8dc074d1a8c8f9904e452a457aeda95eac19
Mailen Zino <mailenzino15@gmail.com>
2021-10-11 18:09:34 -0300

    Tablero.cs

--- TPLabo2-Ajedrez/Program.cs
+++ TPLabo2-Ajedrez/Program.cs
@@ -45,45 +45,82 @@ namespace TPLabo2_Ajedrez
         public sPosicion posicion { get { return posicion; } set { posicion = value; } }
         public ePieza pieza { get { return pieza; } }
    }
-   class Program
-   {
-       int CANT_SOL;
-       sPieza[] LOOKUP;
-       sPieza TORRE1; 
+    class Program
+    {
+        int CANT_SOL;
+        sPieza[] LOOKUP;
+        sPieza TORRE1;
+        sPieza TORRE2;
+        sPieza ALFIL1;
+        sPieza ALFIL2;
+        sPieza REINA;
         public Program()
-        { 
-            CANT_SOL=0;
-            LOOKUP = new sPieza[8];
-        }
-        public void CargarFILACOL()
         {
-
+            CANT_SOL = 0;
+            LOOKUP = new sPieza[8];
         }
         public void BuscarSoluciones()
         {
-            bool[,] MatrizPrueba = new bool[8, 8];
-            // 1 posicion atacada | 0 libre
+            Tablero MatrizPrueba = new Tablero(8);
+            // 1 para posicion atacada | 0 libre
 
-            // Posicionamos las torres para trabajar en un 6x6
-            sPieza TORRE1 = new sPieza(ePieza.TORRE, 0, 0);
-            sPieza TORRE2 = new sPieza(ePieza.TORRE, 1, 1);
+            //                   Cargamos ataque de las torres y guardamos la posicion en look up
+            TORRE1 = new sPieza(ePieza.TORRE, 0, 0);
+            TORRE2 = new sPieza(ePieza.TORRE, 1, 1);
             LOOKUP[0] = TORRE1;
-            LOOKUP[0] = TORRE2;
-            CargarFILACOL(0);
-            CargarFILACOL(1);
-            CargarFILACOL(0,false);
-            CargarFILACOL(1,false);
+            LOOKUP[1] = TORRE2;
+
+            MatrizPrueba.CargarFILACOL(0);
+            MatrizPrueba.CargarFILACOL(1);
+            MatrizPrueba.CargarFILACOL(0, false);
+            MatrizPrueba.CargarFILACOL(1, false);
+
+
+            //                   Cargamos ataque de alfiles y guardamos la posicion en look up 
+
+            Random rd = new Random(); int auxPos = rd.Next(2, 5); //llega a 5 o a 4??
+
+            ALFIL1 = new sPieza(ePieza.ALFIL, auxPos, auxPos);
+
+            if (auxPos == 2)
+                ALFIL2 = new sPieza(ePieza.ALFIL, auxPos, auxPos + 1);
+            else
+                ALFIL2 = new sPieza(ePieza.ALFIL, auxPos, auxPos - 1);
+
+            LOOKUP[2] = ALFIL1;
+            LOOKUP[3] = ALFIL2;
+            //ya nos aseguramos que quede uno en casilla blanca y otro en negra
+
+            MatrizPrueba.CargarDiagonales(ALFIL1.posicion.FILA, ALFIL1.posicion.COL);
+            MatrizPrueba.CargarDiagonales(ALFIL2.posicion.FILA, ALFIL2.posicion.COL);
+
+
+            //                   Cargamos ataque de la reina y guardamos la posicion en look up 
+            int auxCol;
+            int auxFila;
+            do
+            {
+                auxCol = rd.Next(2, 5);
+                auxFila = rd.Next(2, 5);
+            } while (!(MatrizPrueba.VerificarLibredeAtaque(auxFila, auxCol)));
+            REINA = new sPieza(ePieza.REINA, auxFila, auxCol);
+            LOOKUP[4] = REINA;
+            MatrizPrueba.CargarDiagonales(REINA.posicion.FILA, REINA.posicion.COL);
+            MatrizPrueba.CargarFILACOL(auxFila);
+            MatrizPrueba.CargarFILACOL(auxFila, false);
+
+            //seguir con el rey
 
         }
         static void Main(string[] args)
-       {
-            var Tablero=new Program();
+        {
+            var Tablero = new Program();
             while (Tablero.CANT_SOL < 10)
             {
                 Tablero.BuscarSoluciones();
                 Tablero.CANT_SOL++;
             }
-       }
-       
-   }
+        }
+
+    }
 }
---
+++ TPLabo2-Ajedrez/Tablero.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPLabo2_Ajedrez
+{
+    class Tablero
+    {
+        bool[,] Matriz;
+        int N;
+        public Tablero(int n)
+        {
+            bool[,] Matriz = new bool[n, n];
+            N = n;
+        }
+        public void CargarFILACOL(int numero, bool fila = true)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                if (fila)
+                    Matriz[numero, i] = true;
+                else
+                    Matriz[i, numero] = true;
+            }
+        }
+        void CargarDiagonalesInferiores(int fila, int col) { }
+        void CargarDiagonalesSuperiores(int fila, int col) { }
+        public void CargarDiagonales(int fila, int col)
+        {
+            CargarDiagonalesInferiores(fila, col);
+            CargarDiagonalesSuperiores(fila, col);
+
+        }
+        public bool VerificarLibredeAtaque(int fila, int col)
+        {
+            if (Matriz[fila, col])
+                return false;
+            return true;
+        }
+    }
+}
